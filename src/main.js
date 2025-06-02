ce0980346f0655df3fe3e3c626ef963b520e3cd9
phantomvoyager4 <mateusz.pilawski@gmail.com>
2025-06-02 14:18:04 +0200

    feat: initial api setup

--- src/main.js
+++ src/main.js
@@ -22,3 +22,46 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+
+const response = fetch('https://topfkdmhqcnshmzuwert.supabase.co', {
+  headers: {
+  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcGZrZG1ocWNuc2htenV3ZXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM2ODgsImV4cCI6MjA2MzIyOTY4OH0.0k6GPQG6khofQKectWdp7JaFZpLVtA5Gxhv7SOeZNls',
+  },
+  });
+  console.log(response);
+
+  const fetchArticles = async () => {
+    try {
+    const response = await fetch(
+    'https://topfkdmhqcnshmzuwert.supabase.co', {
+    headers: {
+    apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcGZrZG1ocWNuc2htenV3ZXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM2ODgsImV4cCI6MjA2MzIyOTY4OH0.0k6GPQG6khofQKectWdp7JaFZpLVtA5Gxhv7SOeZNls',
+    },
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+    } catch (error) {
+    console.error('Fetch error:', error);
+    }
+    };
+
+    const createNewArticle = async (title) => {
+      try {
+      const response = await fetch('https://topfkdmhqcnshmzuwert.supabase.co', {
+      method: 'POST',
+      headers: {
+      apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcGZrZG1ocWNuc2htenV3ZXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM2ODgsImV4cCI6MjA2MzIyOTY4OH0.0k6GPQG6khofQKectWdp7JaFZpLVtA5Gxhv7SOeZNls',
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title }),
+      });
+      if (response.status !== 201) {
+      throw new Error(`Status: ${response.status}`);
+      }
+      } catch (error) {
+      console.error('Fetch error:', error);
+      }
+      };
+      
