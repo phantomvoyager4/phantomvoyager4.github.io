7b529259ae67e4bc88ab94df8c990b63bbe24414
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2025-08-27 12:27:16 +0200

    fix: removed menu border; added intuitive link underline for menu elements

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,15 +1,15 @@
 import menuclose from './assets/menuclose.png'
 function Menu({ open, onClose }) {
     return (
-        <nav className={`border-r border-t border-b border-white bg-black fixed top-0 left-0 w-50 h-full z-50 transition-transform duration-300 ${open ? 'translate-x-0' : '-translate-x-full'}`}>
+        <nav className={` bg-black fixed top-0 left-0 w-50 h-full z-50 transition-transform duration-300 ${open ? 'translate-x-0' : '-translate-x-full'}`}>
             <div className="flex">
               <img src={menuclose} alt="Close Menu" className="ml-auto pt-2 pr-2 cursor-pointer" onClick={onClose} />
             </div>
-            <ul className="text-white font-InriaSerif text-lg pt-2 pl-2">
-              <li><a href="https://linktr.ee/kashiami" target="_blank">Linktree</a></li>
-              <li><a href="mailto:obywatelnumer04230@gmail.com">Direct contact</a></li>
-              <li><a href="https://github.com/phantomvoyager4/phantomvoyager4.github.io" target="_blank">Github repo</a></li>
-              <li className="cursor-pointer">About me</li>
+            <ul className="text-white font-InriaSerif text-lg pt-2 pl-2 ">
+              <li><a href="https://linktr.ee/kashiami" target="_blank" className="hover:underline">Linktree</a></li>
+              <li><a href="mailto:obywatelnumer04230@gmail.com" className="hover:underline">Direct contact</a></li>
+              <li><a href="https://github.com/phantomvoyager4/phantomvoyager4.github.io" target="_blank" className="hover:underline">Github repo</a></li>
+              <li className="cursor-pointer hover:underline">About me</li>
             </ul>
         </nav>
     );
