a2180c5320338f423e71bf38638a1d4672ed9537
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2025-08-27 16:22:51 +0200

    fix: fixed header quality

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ function Header({ onBurgerClick }) {
   return (
     <header className="fixed top-0 left-0 w-full h-[115px] bg-black flex items-center justify-between px-4">
       <img src={burger} alt="Burger" className="w-24 cursor-pointer" onClick={onBurgerClick} />
-      <div className='font-InriaSerif text-8xl'>Kashiami</div>
+      <div className='font-InriaSerif text-8xl text-white'>Kashiami</div>
       <img src={nightmode} alt="Nightmode" className="w-16" />
     </header>
   );
--- src/cardStorage.js
+++ src/cardStorage.js
@@ -20,9 +20,9 @@ export const cardStorage = [
     logoHref: "https://www.youtube.com/watch?v=0sYTp5ZvdZo&ab_channel=kashiami-Topic",
     streamText: "Check out my newest single:",
     links: [
-      { url: 'https://music.apple.com/us/artist/kashiami/1777722735', img: MusicD },
-      { url: 'https://www.youtube.com/@kashiami4', img: YoutubeD },
-      { url: 'https://open.spotify.com/artist/7yFxrWhhvzk9OQz2aTIhul', img: SpotifyD }
+      { url: 'https://music.apple.com/us/album/prenumbra/1831024899?i=1831024900', img: MusicD },
+      { url: 'https://www.youtube.com/playlist?list=OLAK5uy_lFyjY17jQ-09gw21XHktQn2WM2g1mzaFU', img: YoutubeD },
+      { url: 'https://open.spotify.com/album/6CA4lU88xZqKxjAIzkhYY8?si=uWBTx9dYTc6I1Ho7FizjBA', img: SpotifyD }
     ]
   }
 ];
