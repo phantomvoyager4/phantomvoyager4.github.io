1b82654b72322cb50a4930572ac5ce0eb69be1f4
phantomvoyager4 <mateusz.pilawski@gmail.com>
2025-10-07 14:17:34 +0200

    fix: updated card to the latest song released

--- src/Components/cardStorage.js
+++ src/Components/cardStorage.js
@@ -2,7 +2,7 @@ import LogoJ from './assets/Logoj.jpg';
 import MusicD from './assets/MusicD.png';
 import YoutubeD from './assets/YoutubeD.png';
 import SpotifyD from './assets/SpotifyD.png';
-import Penumbra from './assets/penumbra.jpg';
+import Newwave from './assets/newwave.png';
 
 export const cardStorage = [
   {
@@ -16,13 +16,13 @@ export const cardStorage = [
     ]
   },
   {
-    logo: Penumbra,
-    logoHref: "https://www.youtube.com/watch?v=0sYTp5ZvdZo&ab_channel=kashiami-Topic",
-    streamText: "Check out my newest single:",
+    logo: Newwave,
+    logoHref: "https://www.youtube.com/watch?v=fkKJEUWPAzY&list=OLAK5uy_kSVZJyaUBTUUgtxXE6ixLo-_v5dG8xPZ0",
+    streamText: "Check out my latest single:",
     links: [
-      { url: 'https://music.apple.com/us/album/prenumbra/1831024899?i=1831024900', img: MusicD },
-      { url: 'https://www.youtube.com/playlist?list=OLAK5uy_lFyjY17jQ-09gw21XHktQn2WM2g1mzaFU', img: YoutubeD },
-      { url: 'https://open.spotify.com/album/6CA4lU88xZqKxjAIzkhYY8?si=uWBTx9dYTc6I1Ho7FizjBA', img: SpotifyD }
+      { url: 'https://music.apple.com/pl/album/new-wave-single/1840562785', img: MusicD },
+      { url: 'https://music.apple.com/pl/album/new-wave-single/1840562785', img: YoutubeD },
+      { url: 'https://open.spotify.com/album/1pRpyMMqdeTXaeTh2K4BCJ?si=C-y_gaylSGysTYEn9QHNsQ', img: SpotifyD }
     ]
   }
 ];
