747f3624d094cb562763959995cb2d55184b36b4
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2025-12-21 16:29:54 +0100

    Updates

---
+++ assets/Copyright-Dn8mE_Mt.js
@@ -0,0 +1,2 @@
+import{j as i}from"./animations-PY-NrJsM.js";function r({palette:t}){return i.jsx("div",{className:"font-InriaSerif sticky bottom-2 left-4",style:{color:t.copyrightText},children:"© 2025 Kashiami"})}export{r as C};
+//# sourceMappingURL=Copyright-Dn8mE_Mt.js.map
