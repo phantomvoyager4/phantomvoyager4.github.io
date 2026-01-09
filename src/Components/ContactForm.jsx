d409ddbc10e9e88862ba5319aa2792fe593d5084
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2026-01-09 21:32:00 +0100

    fix: contact button

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -266,20 +266,7 @@ function ContactForm({ palette }) {
                   Sending...
                 </span>
               ) : (
-                <span className="flex items-center justify-center ">
-                  <svg
-                    className="w-5 h-5 mr-2 rotate-90"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                    />
-                  </svg>
+                <span className="items-center justify-center ">
                   Send Message
                 </span>
               )}
